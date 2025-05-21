3e11e0547ee0238a83a96d09307ca8286332ca4e
Ewan-Reveille <ewan.reveille23@gmail.com>
2025-05-21 15:22:56 +0200

    Feat: Added Product Cards and Grid to the main page

--- components/ProductCard.vue
+++ components/ProductCard.vue
@@ -1,5 +1,18 @@
 <template>
     <UCard class="cursor-pointer hover:shadow-lg transition-all">
-        <NuxtImg src="https://picsum.photos/200/300" class="w-full h-auto object-cover" />
+        <NuxtImg :src="product.image" :alt="product.title" class="w-full h-48 object-cover" />
+        <div class="p-4">
+            <h3 class="font-semibold mb-2">{{ product.title }}</h3>
+            <p class="text-sm mb-1">{{ product.category }}</p>
+            <p class="text-lg font-bold">{{ product.price.toFixed(2) }} €</p>
+            <button class="mt-4 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition">
+                Add to Cart
+            </button>
+        </div>
     </UCard>
 </template>
+
+<script setup lang="ts">
+import { type Product } from '~~/types/product'
+const props = defineProps<{ product: Product }>()
+</script>
--- components/ProductGrid.vue
+++ components/ProductGrid.vue
@@ -1,7 +1,7 @@
 <template>
     <h2 class="ml-5 text-xl font-semibold">Our store</h2>
-    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mt-4" v-if="data?.length">
-        <ProductCard v-for="product in data" />
+    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mt-4" v-if="products?.length">
+        <ProductCard v-for="product in products" :key="product.id" :product />
     </div>
     <div v-else>
         <h3 class="w-full text-center my-12">No datas available... For now</h3>
@@ -9,5 +9,22 @@
 </template>
 
 <script setup lang="ts">
-    const { data, error, loading, refresh } = await useFetch('/api/products');
-</script>   
+import { ProductSchema, type Product } from '~~/types/product';
+import z from 'zod';
+const ProductsSchema = z.array(ProductSchema)
+
+const products = ref<Product[]>([]);
+
+onMounted(() => {
+    const datas = localStorage.getItem('products');
+    if (!datas) return;
+    try {
+        const parsed = JSON.parse(datas);
+        console.log(products.value)
+        const validated = ProductsSchema.parse(parsed)
+        products.value = validated
+    } catch (error) {
+        console.error('Invalid product data in localStorage:', error)
+    }
+})
+</script>
